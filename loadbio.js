60effa97a14692ab652dc11a14b8132c0aa0b2d2
Sabrina L <sabrinali0312@gmail.com>
2019-01-31 16:08:47 -0500

    header across three files

---
+++ loadbio.js
@@ -0,0 +1,8 @@
+// window.onload = function() {
+// document.getElementById("bio-main").innerHTML = '<object type="type/html" data="about.html" ></object>';
+// }
+
+// function testfunction() {
+//     alert("got here");
+//   document.getElementById("bio-main").innerHTML = 'about.html';
+// }
